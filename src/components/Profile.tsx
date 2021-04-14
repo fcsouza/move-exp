8fb0a5af3de8e19f40bbbc6af8956aa8c8526917
fcsouza <fcsouza18@hotmail.com>
2021-04-14 19:58:04 -0300

    include profile page

---
+++ src/components/Profile.tsx
@@ -0,0 +1,20 @@
+import { useChallenges } from '../hooks/useChallenges';
+
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+  const { level } = useChallenges();
+
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/fcsouza.png" alt="Fabricio Cavalcante "/>
+      <div>
+        <strong>Fabricio Cavalcante</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level {level}
+        </p>
+      </div>
+    </div>
+  );
+}
